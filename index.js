632d972fe40431c03f8855030834aab2f12f6e45
Hyeonggi <85931677+KHyeonggi@users.noreply.github.com>
2024-09-27 17:47:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-class Calculator {
-    $previousPreview
-    $currentPreview
-    previousOperation = ""
-    currentOpenration = ""
+class Calulcator {
+    $previousPreview = ''
+    $currentPreview = ''
+    previousOperation = ''
+    currentOperation = ''
 
     constructor($previousPreview, $currentPreview) {
         this.$previousPreview = $previousPreview
@@ -10,112 +10,67 @@ class Calculator {
     }
 
     onPressNumber(number) {
-       // 소수점 입력시 현재 입력 받은 데이터가 없는 경우
-       if (number === "." && this.$currentPreview.textContent.length < 1) {
-         return 
-       }
-
-       this.$currentPreview.textContent += number
+        this.$currentPreview.textContent += number
     }
 
     onPressOperation(operation) {
-        // 연산기호 입력시 현재 입력 받은 데이터가 없는 경우
-        if (this.$currentPreview.textContent.length < 1) {
-            return 
-        }
-        this.previousOperation = operation
-        this.$previousPreview.textContent = `${this.$currentPreview.textContent} ${operation}`
+        this.$previousPreview.textContent =
+            this.$currentPreview.textContent + ' ' + operation
         this.$currentPreview.textContent = ''
+        this.previousOperation = operation
     }
 
     onEqual() {
-        if (
-            this.$currentPreview.textContent.length < 1 ||
-            this.$previousPreview.textContent.length < 1 ||
-            this.previousOperation.length < 1
-        ) {
-            return
-        }
-
+        const operation = this.previousOperation.trim()
         let result = 0
 
-        switch (this.previousOperation) {
-            case "+":
-                result = this.handlePlus()
-                break
-            case "-":
-                result = this.handleMinus()
-                break
-            case "*":
-                result = this.handleMultiply()
-                break
-            case "÷":
-                result = this.handleDivide()
-                break
-            default:
-                break
+        if (operation === '+') {
+            result =
+                +this.$previousPreview.textContent.split(' ')[0] +
+                +this.$currentPreview.textContent
+        } else if (operation === '-') {
+            result =
+                +this.$previousPreview.textContent.split(' ')[0] -
+                +this.$currentPreview.textContent
+        } else if (operation === '*') {
+            result =
+                +this.$previousPreview.textContent.split(' ')[0] *
+                +this.$currentPreview.textContent
+        } else if (operation === '÷') {
+            result =
+                +this.$previousPreview.textContent.split(' ')[0] /
+                +this.$currentPreview.textContent
         }
+
+        this.$previousPreview.textContent = ''
         this.$currentPreview.textContent = result.toString()
-        this.$previousPreview.textContent = ""
-        this.currentOperation = ""
-    }
-
-    handlePlus() {
-        return (
-            Number(this.$previousPreview.textContent.split(" ")[0])
-            + Number(this.$currentPreview.textContent)
-        )
-    }
-
-    handleMinus() {
-        return (
-            Number(this.$previousPreview.textContent.split(" ")[0])
-            - Number(this.$currentPreview.textContent)
-        )
-    }
-
-    handleMultiply() {
-        return (
-            Number(this.$previousPreview.textContent.split(" ")[0])
-            * Number(this.$currentPreview.textContent)
-        )
-    }
-
-    handleDivide() {
-        return (
-            Number(this.$previousPreview.textContent.split(" ")[0])
-            / Number(this.$currentPreview.textContent)
-        )
+        this.currentOperation = ''
     }
 
     onReset() {
-        this.$currentPreview.textContent = ""
-        this.$previousPreview.textContent = ""
-        this.previousOperation = ""
-        this.currentOpenration = ""
+        this.$previousPreview.textContent = ''
+        this.$currentPreview.textContent = ''
+        this.previousOperation = ''
+        this.currentOperation = ''
     }
 
     onDelete() {
-        if (this.$currentPreview.textContent < 1) {
-            return
-        }
-
-        this.$currentPreview.textContent = this.$currentPreview.textContent.slice(0, -1)
+        this.$currentPreview.textContent =
+            this.$currentPreview.textContent.slice(0, -1)
     }
 }
 
-// 값 표시
-const $previousPreview = document.querySelector('[data-previous-preview]')
-const $currentPreview = document.querySelector('[data-current-preview]')
-
+// DOM
 // 사칙연산
 const $plus = document.querySelector('[data-btn-plus]')
 const $minus = document.querySelector('[data-btn-minus]')
-const $multiply = document.querySelector('[data-btn-multiply]')
 const $divide = document.querySelector('[data-btn-divide]')
-const $eqaul = document.querySelector('[data-btn-eqaul]')
+const $multifly = document.querySelector('[data-btn-multifly]')
 
-// 리셋, 삭제
+// 연산
+const $equal = document.querySelector('[data-btn-equal]')
+
+// 전체삭제(AC), 삭제
 const $reset = document.querySelector('[data-btn-reset]')
 const $delete = document.querySelector('[data-btn-delete]')
 
@@ -123,37 +78,33 @@ const $delete = document.querySelector('[data-btn-delete]')
 const $numbers = document.querySelectorAll('[data-btn-number]')
 const $operations = document.querySelectorAll('[data-btn-operation]')
 
-const cal = new Calculator($previousPreview, $currentPreview)
+// 프롬프트
+const $previousPreview = document.querySelector('[data-previous-preview]')
+const $currentPreview = document.querySelector('[data-current-preview]')
+
+// 연산 처리
+const calc = new Calulcator($previousPreview, $currentPreview)
 
 $numbers.forEach(($number) => {
-    $number.addEventListener("click", (e) => {
-        cal.onPressNumber(e.target.textContent)
+    $number.addEventListener('click', (e) => {
+        calc.onPressNumber(e.target.textContent)
     })
 })
 
 $operations.forEach(($operation) => {
-    $operation.addEventListener("click", (e) => {
-       switch ($operation) {
-            case $plus:
-                cal.onPressOperation("+")
-                break;
-            case $minus:
-                cal.onPressOperation("-")
-                break;
-            case $multiply:
-                cal.onPressOperation("*")
-                break;
-            case $divide:
-                cal.onPressOperation("÷")
-                break;
-            case $eqaul:
-                cal.onEqual()
-                break;
-            default:
-                break;
-       }
+    $operation.addEventListener('click', (e) => {
+        if (e.target.textContent.trim() === '=') {
+            calc.onEqual()
+        } else {
+            calc.onPressOperation(e.target.textContent)
+        }
     })
 })
 
-$reset.addEventListener("click", (e) => cal.onReset())
-$delete.addEventListener("click", (e) => cal.onDelete())
+$reset.addEventListener('click', (e) => {
+    calc.onReset()
+})
+
+$delete.addEventListener('click', (e) => {
+    calc.onDelete()
+})
